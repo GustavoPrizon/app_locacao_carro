38969ad512928869c6bd3c20726ab5088d70559d
gustavoprizon <gustavoprizon@gmail.com>
2022-04-06 14:00:47 -0300

    complete details screen

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
 import { ImageSlider } from '../../components/ImageSlider';
 
-import {Container, Header, CarImages} from './styles';
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExChangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import {Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer
+} from './styles';
+import { Button } from '../../components/Button';
 
 export function CarDetails(){
   return (
@@ -11,9 +33,41 @@ export function CarDetails(){
       <Header>
         <BackButton onPress={() => {}}/>
       </Header>
+
       <CarImages>
         <ImageSlider imagesUrl={['https://img1.gratispng.com/20180928/pka/kisspng-2-18-audi-s5-3-t-premium-plus-coupe-car-dealershi-audi-s5-2-18-view-specs-prices-photos-more-5bae7df7d53507.4261876315381621678733.jpg',]}/>
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Audi</Brand>
+            <Name>RS 5 Coupe</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 120</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg}/>
+          <Accessory name="3.2s" icon={AccelerationSvg}/>
+          <Accessory name="800 HP" icon={ForceSvg}/>
+          <Accessory name="Gasoline" icon={GasolineSvg}/>
+          <Accessory name="Auto" icon={ExChangeSvg}/>
+          <Accessory name="2 pessoas" icon={PeopleSvg}/>
+        </Accessories>
+
+        <About>
+          Este é automóvel desportivo. 
+          Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. 
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
+      <Footer>
+        <Button title='Confirmar'/>
+      </Footer>
     </Container>
   );
 }
