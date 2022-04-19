5ad95e88ad753b693016c46424e26c06e270a863
Gustavo <gustavoprizon@gmail.com>
2022-04-19 13:47:57 -0300

    add margin bottom on button component

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,13 +1,13 @@
-import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
-import { RFValue } from 'react-native-responsive-fontsize';
-import styled from 'styled-components/native';
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
 
 interface ButtonProps extends RectButtonProps {
-  color: string
+  color: string;
 }
 
-interface ButtonTextProps{
-  light: boolean
+interface ButtonTextProps {
+  light: boolean;
 }
 
 export const Container = styled(RectButton)<ButtonProps>`
@@ -15,12 +15,15 @@ export const Container = styled(RectButton)<ButtonProps>`
   padding: 19px;
   align-items: center;
   justify-content: center;
-  background-color: ${({color, theme}) => color ? color : theme.colors.main};
+  background-color: ${({ color, theme }) =>
+    color ? color : theme.colors.main};
   border-radius: 8px;
+  margin-bottom: 8px;
 `;
 
 export const Title = styled.Text<ButtonTextProps>`
-  font-family: ${({theme}) => theme.fonts.primary_500};
+  font-family: ${({ theme }) => theme.fonts.primary_500};
   font-size: ${RFValue(15)}px;
-  color: ${({theme, light}) => light ? theme.colors.header : theme.colors.shape};
+  color: ${({ theme, light }) =>
+    light ? theme.colors.header : theme.colors.shape};
 `;
