3f16e1d009b1e233e52c05eadf93c590c31b8af8
gustavoprizon <gustavoprizon@gmail.com>
2022-04-21 16:53:55 -0300

    accessibilite on form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,66 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { useTheme } from 'styled-components';
-import {Button} from '../../components/Button';
-import { Input } from '../../components/Input';
-import { PasswordInput } from '../../components/PasswordInput';
-
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 import {
-  Container,
-  Header,
-  Title,
-  SubTitle,
-  Form,
-  Footer
-} from './styles';
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+import { useTheme } from "styled-components";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
+import { PasswordInput } from "../../components/PasswordInput";
 
-export function Signin(){
+import { Container, Header, Title, SubTitle, Form, Footer } from "./styles";
+
+export function Signin() {
   const theme = useTheme();
 
   return (
-    <Container>
-      <StatusBar style='dark' translucent/>
-      <Header>
-        <Title>
-          Estamos {`\n`}
-          quase lá.
-        </Title>
-        <SubTitle>
-          Faça seu login para começar {`\n`}
-          uma experiência incrível.
-        </SubTitle>
-      </Header>
-      <Form>
-        <Input 
-          iconName='mail' 
-          placeholder='E-mail' 
-          keyboardType='email-address'
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
-        <PasswordInput 
-          iconName='lock'
-          placeholder='Senha'
-          autoCorrect={false}
-          autoCapitalize='none'
-        />
-      </Form>
-      <Footer>
-        <Button 
-          title='Login'
-          onPress={() => {}} 
-          enabled={false} 
-          load={false}
-        />
-        <Button 
-          title='Criar conta gratuita'
-          onPress={() => {}} 
-          enabled={true} 
-          load={false}
-          color={theme.colors.background_secondary}
-          light
-        />
-      </Footer>
-    </Container>
+    <KeyboardAvoidingView behavior="position" enabled>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <Container>
+          <StatusBar style="dark" translucent />
+          <Header>
+            <Title>
+              Estamos {`\n`}
+              quase lá.
+            </Title>
+            <SubTitle>
+              Faça seu login para começar {`\n`}
+              uma experiência incrível.
+            </SubTitle>
+          </Header>
+          <Form>
+            <Input
+              iconName="mail"
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+            <PasswordInput
+              iconName="lock"
+              placeholder="Senha"
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+          </Form>
+          <Footer>
+            <Button
+              title="Login"
+              onPress={() => {}}
+              enabled={false}
+              load={false}
+            />
+            <Button
+              title="Criar conta gratuita"
+              onPress={() => {}}
+              enabled={true}
+              load={false}
+              color={theme.colors.background_secondary}
+              light
+            />
+          </Footer>
+        </Container>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -1,11 +1,10 @@
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
-import { RFValue } from 'react-native-responsive-fontsize';
-import styled from 'styled-components/native';
+import { getStatusBarHeight } from "react-native-iphone-x-helper";
+import { RFValue } from "react-native-responsive-fontsize";
+import styled from "styled-components/native";
 
 export const Container = styled.View`
-  flex: 1;
   padding: 0 24px;
-  background-color: ${({theme}) => theme.colors.background_primary};
+  background-color: ${({ theme }) => theme.colors.background_primary};
 `;
 
 export const Header = styled.View`
@@ -15,14 +14,14 @@ export const Header = styled.View`
 
 export const Title = styled.Text`
   font-size: ${RFValue(40)}px;
-  font-family: ${({theme}) => theme.fonts.secondary_600};
-  color: ${({theme}) => theme.colors.title};
+  font-family: ${({ theme }) => theme.fonts.secondary_600};
+  color: ${({ theme }) => theme.colors.title};
 `;
 
 export const SubTitle = styled.Text`
   font-size: ${RFValue(15)}px;
-  font-family: ${({theme}) => theme.fonts.primary_400};
-  color: ${({theme}) => theme.colors.text};
+  font-family: ${({ theme }) => theme.fonts.primary_400};
+  color: ${({ theme }) => theme.colors.text};
   line-height: ${RFValue(25)}px;
   margin-top: 16px;
 `;
@@ -32,6 +31,4 @@ export const Form = styled.View`
   margin: 64px 0;
 `;
 
-export const Footer = styled.View`
-
-`;
+export const Footer = styled.View``;
